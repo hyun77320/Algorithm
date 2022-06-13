55f868071e237ed71b1c96a7539481ee7e760655
iqamac <hyun77320@naver.com>
2022-06-13 14:37:38 +0900

    2022.06.13.

--- 17614.py
+++ 17614.py
@@ -1,9 +1,8 @@
 n = int(input())
-count = 0
+cnt = 0
 
-for i in range(3, n+1) :
-  count += str(i).count('3')
-  count += str(i).count('6')
-  count += str(i).count('9')
+for i in range(1, n+1):
+    i_str = str(i)
+    cnt += i_str.count("3") + i_str.count("6") + i_str.count("9")
 
-print(count)
+print(cnt)
